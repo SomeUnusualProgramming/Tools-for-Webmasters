93c11a43ff17869de4245ffba3db4505a8ee47ed
SomeUnusualProgramming <swierkosz.adam@gmail.com>
2019-08-19 15:19:46 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -45,6 +45,76 @@ const context = {
         site4: 'Fontello',
         link4: 'http://fontello.com/'
     },
+    {
+        href: '#thirdSubmenu',
+        title: 'Colors',
+        site1: 'Adobe Color',
+        link1: 'https://color.adobe.com/pl/create/color-wheel/',
+        site2: 'Colorzilla',
+        link2: 'https://www.colorzilla.com/gradient-editor/',
+        site3: 'FXXX',
+        link3: 'https://fontawesome.com/icons?d=gallery'
+    },
+    {
+        href: '#fourthSubmenu',
+        title: 'Snippets',
+        site1: 'Stack overflow',
+        link1: 'https://stackoverflow.com/',
+        site2: 'Snipplr',
+        link2: 'https://snipplr.com/',
+        site3: 'XXX',
+        link3: 'https://fontawesome.com/icons?d=gallery'
+    },
+    {
+        href: '#fifthSubmenu',
+        title: 'Code Editors',
+        site1: 'XXX Visual',
+        link1: 'https://stackoverflow.com/',
+        site2: 'XXX Brackets',
+        link2: 'https://snipplr.com/',
+        site3: 'XXX Atom',
+        link3: 'https://fontawesome.com/icons?d=gallery'
+    },
+    {
+        href: '#sixthSubmenu',
+        title: 'Validators',
+        site1: 'W3C Validator',
+        link1: 'https://validator.w3.org/',
+        site2: 'Mobile optimialization by Google',
+        link2: 'https://search.google.com/test/mobile-friendly',
+        site3: 'Page Speed test',
+        link3: 'https://tools.pingdom.com/'
+    },
+    {
+        href: '#seventhSubmenu',
+        title: 'Image compresion',
+        site1: 'JPEG Mini',
+        link1: 'https://www.jpegmini.com/',
+        site2: 'Tiny PNG',
+        link2: 'https://tinypng.com/',
+        site3: 'XXX',
+        link3: 'https://tools.pingdom.com/'
+    },
+    {
+        href: '#eigthSubmenu',
+        title: 'Online Editors',
+        site1: 'Codepen',
+        link1: 'https://codepen.io/',
+        site2: 'Liveweave',
+        link2: 'https://liveweave.com/',
+        site3: 'XXX',
+        link3: 'https://tools.pingdom.com/'
+    },
+    {
+        href: '#ninethSubmenu',
+        title: 'RWD',
+        site1: 'Active Demo',
+        link1: 'https://activedemo.net/viewport-test',
+        site2: 'Quirktools',
+        link2: 'http://quirktools.com/screenfly',
+        site3: 'XXX',
+        link3: 'https://tools.pingdom.com/'
+    },
 
 
 
